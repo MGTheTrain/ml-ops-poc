3d23a59b764b01e55d76ab6a003697ca7eadfbd3
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-24 19:06:19 +0200

    utilize existing terraform module for aks deployment

---
+++ terraform/envs/sbx-k8s-deployment/main.tf
@@ -0,0 +1,16 @@
+module "main" {
+    source = "https://github.com/MGTheTrain/gitops-ftw.git//terraform/modules/az?ref=master"
+    digital_product_affix = var.digital_product_affix
+    environment = var.environment
+    resource_instance_number = var.resource_instance_number
+    location = var.location
+    team = var.team
+    virtual_network_address_space = var.virtual_network_address_space
+    number_of_aks = var.number_of_aks
+    aks_admin_username = var.aks_admin_username
+    aks_enable_auto_scaling = var.aks_enable_auto_scaling
+    aks_node_count = var.aks_node_count
+    aks_max_node_count = var.aks_max_node_count
+    aks_vm_size = var.aks_vm_size
+    aks_os_disk_size_gb = var.aks_os_disk_size_gb
+}
