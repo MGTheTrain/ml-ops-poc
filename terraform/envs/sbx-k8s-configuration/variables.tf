e52a1ecc0635520d9b796f2cab7f7dbbe40a5c39
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-25 22:34:42 +0200

    update validation for ml_ops_tool input var

--- terraform/envs/sbx-k8s-configuration/variables.tf
+++ terraform/envs/sbx-k8s-configuration/variables.tf
@@ -24,7 +24,7 @@ variable "acr_password" {
 }
 
 variable "ml_ops_tool" {
-  description = "String determining whether to install mlflow or none. Viable options: [ mlflow, none ]. The installation of Kubeflow will be managed externally through the continuous delivery (CD) workflow, as Terraform modules and kubernetes provider are either outdated or difficult to setup"
+  description = "String determining whether to install mlflow or none. Viable options: [ mlflow, kubeflow, none ]. The installation of Kubeflow will be managed externally through the continuous delivery (CD) workflow, as Terraform modules and kubernetes provider are either outdated or difficult to setup"
   type        = string
   default     = "none"
 }
