d0b299be988645ed4cddd13be5a693181682d48b
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-27 14:58:58 +0200

    save trained model

--- python/keras-mnist-training/src/train.py
+++ python/keras-mnist-training/src/train.py
@@ -1,3 +1,4 @@
+import os
 from .data_loader import load_data
 from .model import build_model
 
@@ -15,3 +16,9 @@ def main():
     
     # Train model using training data
     model.fit(x_train, y_train, epochs=10, batch_size=32)
+
+    # Save trained model to models folder
+    model_dir = "../models"
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    model.save(os.path.join(model_dir, "mnist_model.h5"))
