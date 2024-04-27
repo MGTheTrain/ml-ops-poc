e6dd2f7c48504412650524ea0b05c5e97b073e72
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-27 14:56:31 +0200

    fix simple feed forward network sample with mnist dataset

--- python/keras-mnist-training/src/train.py
+++ python/keras-mnist-training/src/train.py
@@ -2,10 +2,16 @@ from .data_loader import load_data
 from .model import build_model
 
 def main():
-    x_train, y_train = load_data()
+    # Load data
+    (x_train, y_train), (x_test, y_test) = load_data()
+    
+    # Build model
     model = build_model()
+    
+    # Compile model
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     
+    # Train model using training data
     model.fit(x_train, y_train, epochs=10, batch_size=32)
