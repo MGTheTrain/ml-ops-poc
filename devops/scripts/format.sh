80d0781808baea68405e5e6016c9ef33a420df27
Marvin Gajek <Marvin.Gajek1@web.de>
2025-01-29 21:18:12 +0100

    modify format.sh script

--- devops/scripts/format.sh
+++ devops/scripts/format.sh
@@ -13,11 +13,31 @@ NC='\033[0m'
 echo "#####################################################################################################"
 echo -e "$BLUE INFO: $NC About to auto-format hcl files and generate docs for hcl files"
 
-TF_ENVS_FOLDER="./terraform/envs"
-TF_MODULES_FOLDER="./terraform/modules"
-DEVOPS_TF_FOLDER="./devops/terraform"
-for dir in ${TF_ENVS_FOLDER}/sbx-k8s-deployment ${TF_ENVS_FOLDER}/sbx-k8s-configuration ${TF_MODULES_FOLDER}/k8s ${DEVOPS_TF_FOLDER} ; do
-  terraform-docs markdown table --output-file README.md --hide providers --output-mode replace "$dir"
+folders=(
+  "./devops/terraform/acr"
+  "./devops/terraform/tf-backend"
+)
+
+for folder in "${folders[@]}"; do
+  terraform-docs markdown table --output-file README.md --hide providers --output-mode replace "$folder"
+done
+
+# Define the parent directories
+folders=(
+  "./terraform/envs/sbx/"
+  "./terraform/envs/dev/"
+  "./terraform/envs/qas/"
+  "./terraform/envs/staging/"
+  "./terraform/envs/prd/"
+  "./terraform/modules/"
+)
+
+for parent_folder in "${folders[@]}"; do
+  for folder in "$parent_folder"*/; do
+    if [ -d "$folder" ]; then
+      terraform-docs markdown table --output-file README.md --hide providers --output-mode replace "$folder"
+    fi
+  done
 done
 
 terraform fmt -recursive .
