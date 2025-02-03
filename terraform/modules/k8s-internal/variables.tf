555aa9a89a10ce8f265784b1ff8898c21f2287d5
Marvin Gajek <Marvin.Gajek1@web.de>
2025-02-03 22:28:20 +0100

    mark variables as sensitive

--- terraform/modules/k8s-internal/variables.tf
+++ terraform/modules/k8s-internal/variables.tf
@@ -13,29 +13,34 @@ variable "environment" {
 variable "acr_login_server_name" {
   default     = "TBD"
   sensitive   = true
+  type        = string
   description = "Sets an ACR registry server name."
 }
 
 variable "acr_username" {
   default     = "TBD"
   sensitive   = true
+  type        = string
   description = "Sets an ACR user name."
 }
 
 variable "acr_password" {
   default     = "TBD"
   sensitive   = true
+  type        = string
   description = "Sets an ACR password."
 }
 
 variable "az_sa_connection_string" {
   default     = "TBD"
+  sensitive   = true
   type        = string
   description = "The Azure Storage Account connection string"
 }
 
 variable "az_sa_container_name" {
   default     = "TBD"
+  sensitive   = true
   type        = string
   description = "The Azure Storage Account container name"
 }
