fd2619ecdc704e7ff1ce35c3f2c313233dc70971
Marvin Gajek <Marvin.Gajek1@web.de>
2025-02-03 22:30:11 +0100

    ensure variables exist in parent module

--- terraform/envs/sbx/k8s-internal/main.tf
+++ terraform/envs/sbx/k8s-internal/main.tf
@@ -1,7 +1,9 @@
 module "k8s_internal" {
-  source                = "../../../modules/k8s-internal"
-  environment           = var.environment
-  acr_login_server_name = var.acr_login_server_name
-  acr_username          = var.acr_username
-  acr_password          = var.acr_password
+  source                  = "../../../modules/k8s-internal"
+  environment             = var.environment
+  acr_login_server_name   = var.acr_login_server_name
+  acr_username            = var.acr_username
+  acr_password            = var.acr_password
+  az_sa_connection_string = var.az_sa_connection_string
+  az_sa_container_name    = var.az_sa_container_name
 }
